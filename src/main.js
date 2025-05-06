b45c945d98070331c232f4495384edc48e7ce3e4
Erik <erik.hultqvist@gritacademy.se>
2025-05-06 12:52:35 +0200

    second commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 console.log("Testar logga");
+
+const img = document.createElement('img');
+document.body.append(img);
+
+
+// new URL(relativa sökvägen till bilden, sökväg till modulen)
+// OBS! De två argumenten kan inte vara variabler
+const imgUrl = new URL('./media/kostym.jpg', import.meta.url);
+img.src = imgUrl.href;
